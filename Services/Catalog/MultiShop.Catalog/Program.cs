ec438d512a57ecec2fde57251269f52557074000
msayrac <msayracode@gmail.com>
2024-12-07 18:52:21 +0300

    Controllers added

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -1,4 +1,30 @@
+using Microsoft.Extensions.Options;
+using MultiShop.Catalog.Entities;
+using MultiShop.Catalog.Services.CategoryServices;
+using MultiShop.Catalog.Services.ProductDetails;
+using MultiShop.Catalog.Services.ProductDetailServices;
+using MultiShop.Catalog.Services.ProductImageServices;
+using MultiShop.Catalog.Services.ProductServices;
+using MultiShop.Catalog.Settings;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryServices, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductDetailService, ProductDetailService>();
+builder.Services.AddScoped<IProductImageService, ProductImageService>();
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddScoped<IDatabaseSettings>(sp =>
+{
+	return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSetting"));
+
+
+
 
 // Add services to the container.
 
